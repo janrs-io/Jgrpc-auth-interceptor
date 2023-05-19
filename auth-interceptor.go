d0be241de0ef54e08c4a7c6f5ba45b052d4c2d23
yjy86868 <yjy86868@163.com>
2023-05-19 13:06:57 +0800

    init commit

--- auth-interceptor.go
+++ auth-interceptor.go
@@ -2,27 +2,7 @@ package Jgrpc_auth_interceptor
 
 import (
 	"context"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
-
-// Validator 实现 PGV Validator 接口
-type Validator interface {
-	ValidateAll() error
-}
-
-// ValidationUnaryInterceptor PGV 验证拦截器
-func ValidationUnaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-
-	if r, ok := req.(Validator); ok {
-		if err = r.ValidateAll(); err != nil {
-			return nil, status.Error(codes.InvalidArgument, err.Error())
-		}
-	}
-	return handler(ctx, req)
-
-}
 
 // AuthenticationInterceptor 权限验证拦截器
 func AuthenticationInterceptor(ctx context.Context) (context.Context, error) {
